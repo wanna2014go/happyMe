10fc4cbc53b1a86e58b96c2a6c52b660fc0b08a5
na.wan <na.wan@hpe.com>
2016-09-06 10:17:28 +0800

    created happyMe

---
+++ happyMe/src/main/java/com/wna/happyMe/App.java
@@ -0,0 +1,13 @@
+package com.wna.happyMe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
